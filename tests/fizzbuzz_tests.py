7f31473836bf0a7467440ffc8b9ab8c778433ca7
fukiran <macdorphoto@yahoo.com>
2020-11-26 12:04:30 +0000

    next test %15

--- tests/fizzbuzz_tests.py
+++ tests/fizzbuzz_tests.py
@@ -20,5 +20,8 @@ class TestFizzBuzz(unittest.TestCase):
     def test_fizzbuzz_15_return_fizzbuzz(self):
         self.assertEqual('fizzbuzz',fizzbuzz(15))
 
+    def test_fizzbuzz_45_return_fizzbuzz(self):
+        self.assertEqual('fizzbuzz',fizzbuzz(45))
+
     def test_fizzbuzz_4_return_4str(self):
         self.assertEqual('4',fizzbuzz(4))
